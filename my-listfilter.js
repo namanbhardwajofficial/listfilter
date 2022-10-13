bd7bd51440e7df0088c84e2863c11e7cfabe2298
Naman Bhardwaj <namanbhardwaj.official@gmail.com>
2022-10-13 14:18:16 +0530

    Created basic list filter element

---
+++ my-listfilter.js
@@ -0,0 +1,61 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+
+
+/**
+ * `my-listfilter`
+ * dfdf
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class MyListfilter extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+        .hidden {
+          display: none;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+      <input value="{{prop1::input}}">
+      <ul id="list">
+  <li>Hypertext Markup Language</li>
+  <li>Cascading Style Sheets</li>
+  <li>Javascript</li>
+  <li>Web Components</li>
+  <li>Polymer</li>
+  <li>Web Browser</li>
+  <li>Mentally Friendly</li>
+</ul>
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: '',
+        observer: 'filterList'
+      },
+    };
+  }
+   filterList() {
+    var items = this.$.list.children;
+    console.log(items)
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i];
+      console.log(item)
+      if (item.textContent.includes(this.prop1)) {
+        item.classList.remove('hidden');
+      } else {
+        item.classList.add('hidden');
+      }
+    }
+  }
+}
+
+window.customElements.define('my-listfilter', MyListfilter);
